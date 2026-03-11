61f2fbdfa63c879eef1e97a851c8ee45b36fe913
jaguar-ks <fahdamineksouss@gmail.com>
2026-03-11 13:37:32 +0000

    [UPDATE] : an intro to collectors and how to return a list

--- FunctionalProgramming/FP02Functional.java
+++ FunctionalProgramming/FP02Functional.java
@@ -1,16 +1,25 @@
-import java.util.Comparator;
+// import java.util.Comparator;
 import java.util.List;
-// import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class FP02Functional {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(1, 4, 2, 5, 3);
-        int sum = addListFunctional(numbers);
-        System.out.println(sum);
+        List<Integer> squarNumbers = squarAllNumbers(numbers);
+        System.out.println(squarNumbers);
+        // int sum = addListFunctional(numbers);
+        // System.out.println(sum);
         List<String> name = List.of("Fahd", "Amine", "Ksouss");
-        name.stream()
-            .sorted(Comparator.comparing(x -> x.length()))
-            .forEach(System.out::println);
+        System.out.println(name.stream()
+            .map(n -> n.length())
+            .collect(Collectors.toList()));
+    }
+
+    private static List<Integer> squarAllNumbers(List<Integer> numbers) {
+        return numbers.stream()
+            .map(x -> x * x)
+            .sorted()
+            .collect(Collectors.toList());
     }
 
     private static int addListFunctional(List<Integer> numbers) {
