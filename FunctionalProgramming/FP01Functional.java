a0258dea864713caa52486551a7ac0b0479df5f8
jaguar-ks <fahdamineksouss@gmail.com>
2026-03-11 12:05:40 +0000

    [FIX] : corrected the file and the class name

--- FunctionalProgramming/FP01Functional.java
+++ FunctionalProgramming/FP01Functional.java
@@ -1,10 +1,11 @@
 import java.util.List;
 
-public class FP01Fonctional {
+public class FP01Functional {
     public static void main(String[] args) {
         List<String> courses =
             List.of("Spring", "Spring Boot", "API", "Microservices",
                 "AWS", "PCF", "Azure", "Docker", "Kubernetes");
+        List<Integer> numbers = List.of(12, 3, 6, 17, 4, 12, 30, 15);
         // printAllNumbersInListFonctional(List.of(12, 3, 6, 17, 4, 12, 30));
         // System.out.println("-----------------------------");
         // printEvenNumbersInListFonctional(List.of(12, 3, 6, 17, 4, 12, 30));
@@ -13,7 +14,17 @@ public class FP01Fonctional {
         System.out.println("-----------------------------");
         printCoursesWithSpringInListFonctional(courses);
         System.out.println("-----------------------------");
+        courses.stream()
+            .map(course -> course.length())
+            .forEach(System.out::println);
+        System.out.println("-----------------------------");
         printCoursesLongerThan4(courses);
+        System.out.println("-----------------------------");
+        numbers.stream()
+            .filter(number -> number % 2 != 0)
+            .map(number -> number * number * number)
+            .forEach(System.out::println);
+        System.err.println(Integer.MAX_VALUE);
     }
 
     // private static void printAllNumbersInListFonctional(List<Integer> numbers) {
