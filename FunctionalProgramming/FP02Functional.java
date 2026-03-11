656a94a76b203198b495e12c75d8d1025b9da087
jaguar-ks <fahdamineksouss@gmail.com>
2026-03-11 12:31:08 +0000

    [ADD] : learning reduce operation and accumulaton

---
+++ FunctionalProgramming/FP02Functional.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.function.Function;
+
+public class FP02Functional {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 4, 2, 5, 3);
+        int sum = addListFunctional(numbers);
+        System.out.println(sum);
+    }
+
+    private static int addListFunctional(List<Integer> numbers) {
+        return numbers.stream()
+            .reduce(0, (a, b) -> a + b);
+    }
+}
