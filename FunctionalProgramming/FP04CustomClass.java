b034441c2d42ba8832f0ce86dc10f563a7f06460
jaguar-ks <fahdamineksouss@gmail.com>
2026-03-11 22:21:20 +0000

    [ADD] : playing around with getters setters and defining comparators and predicates

---
+++ FunctionalProgramming/FP04CustomClass.java
@@ -0,0 +1,93 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
+class Course {
+    private String name;
+    private String category;
+    private int reviewScore;
+    private int noOfStudent;
+    
+    public Course(String name, String category, int reviewScore, int noOfStudent) {
+        this.name = name;
+        this.category = category;
+        this.reviewScore = reviewScore;
+        this.noOfStudent = noOfStudent;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getReviewScore() {
+        return reviewScore;
+    }
+
+    public void setReviewScore(int reviewScore) {
+        this.reviewScore = reviewScore;
+    }
+
+    public int getNoOfStudent() {
+        return noOfStudent;
+    }
+
+    public void setNoOfStudent(int noOfStudent) {
+        this.noOfStudent = noOfStudent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString () {
+        return name + ":" + noOfStudent + ":" + reviewScore;
+    }
+};
+
+
+public class FP04CustomClass {
+    public static void main(String [] args) {
+        List<Course> courses = List.of(
+            new Course("React", "Framework", 95, 1200),
+            new Course("Django", "Framework", 98, 1200),
+            new Course("RestAPI", "WebDev", 78, 900),
+            new Course("AWS", "Cloud", 93, 720),
+            new Course("Docker", "Contenirization", 77, 210)
+        );
+        
+        Predicate<Course> ReviewScoreGraterThan90 = course -> course.getReviewScore() > 90;
+        
+        System.out.println(
+            courses.stream()
+            .allMatch(ReviewScoreGraterThan90)
+        );
+        System.out.println(
+            courses.stream()
+            .noneMatch(ReviewScoreGraterThan90)
+        );
+        System.out.println(
+            courses.stream()
+            .anyMatch(ReviewScoreGraterThan90)
+        );
+
+        Comparator<Course> comparingByStudentNumber
+                    = Comparator.comparing(Course::getNoOfStudent)
+                        .thenComparing(Course::getReviewScore)
+                        .reversed();
+        
+        System.out.println(
+            courses.stream()
+            .sorted(comparingByStudentNumber)
+            .limit(3)
+            .toList()
+        );
+    };
+};
